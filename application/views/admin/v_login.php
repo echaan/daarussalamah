793486d12d8e1cb66c4b954dc721394a52e32563
echan <akkitherythm@gmail.com>
2024-10-29 17:18:22 +0700

    Ubah tampilan Login

--- application/views/admin/v_login.php
+++ application/views/admin/v_login.php
@@ -1,32 +1,53 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Yayasan Daarussalamah | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
-  <!-- Bootstrap 3.3.6 -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="shortcut icon" href="<?php echo base_url().'theme/images/icon3.png'?>">
   <link rel="stylesheet" href="<?php echo base_url().'assets/bootstrap/css/bootstrap.min.css'?>">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/font-awesome/css/font-awesome.min.css'?>">
-  <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/dist/css/AdminLTE.min.css'?>">
-  <!-- iCheck -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/plugins/iCheck/square/blue.css'?>">
-
-
+  <style>
+    /* Center the login page with flexbox */
+    .login-page {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+      margin: 0;
+      overflow: hidden;
+    }
+    .login-box {
+      max-width: 400px;
+    }
+    .login-logo img {
+      width: 20%;
+    }
+    .login-logo h3 {
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 15px;
+    }
+    #loginErrorMessage {
+      color: red;
+      text-align: center;
+      margin-bottom: 15px;
+    }
+  </style>
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
-  <div id="loginErrorMessage">
-   <p><?php echo $this->session->flashdata('message_login_error');?></p>
+  <div class="login-logo">
+    <img src="<?php echo base_url().'theme/images/icon3.png'?>" alt="logo-sekolah">
+    <h3>Yayasan Pondok Pesantren Daarussalamah</h3>
   </div>
-  <!-- /.login-logo -->
   <div class="login-box-body">
-    
-
+    <div id="loginErrorMessage">
+      <p><?php echo $this->session->flashdata('message_login_error');?></p>
+    </div>
     <form action="<?php echo site_url().'admin/login/auth'?>" method="post">
       <div class="form-group has-feedback">
         <input type="text" name="username" class="form-control" placeholder="Username" required>
@@ -37,35 +58,18 @@
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-success btn-block btn-flat">Sign In</button>
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
       </div>
     </form>
-
-
-    <!-- /.social-auth-links -->
     <hr/>
-    <p><center>Copyright <?php echo date('Y');?> by MTs Tarbiyatul Falah <br/> All Right Reserved</center></p>
+    <p class="text-center">Copyright &copy; <?php echo date('Y');?> by Daarussalamah. All rights reserved.</p>
   </div>
-  <!-- /.login-box-body -->
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery 2.2.3 -->
 <script src="<?php echo base_url().'assets/plugins/jQuery/jquery-2.2.3.min.js'?>"></script>
-<!-- Bootstrap 3.3.6 -->
 <script src="<?php echo base_url().'assets/bootstrap/js/bootstrap.min.js'?>"></script>
-<!-- iCheck -->
 <script src="<?php echo base_url().'assets/plugins/iCheck/icheck.min.js'?>"></script>
 <script>
   $(function () {
@@ -76,11 +80,10 @@
     });
   });
   $(document).ready(function(){
-            // Menghilangkan alert setelah 3 detik
-            setTimeout(function(){
-                $("#loginErrorMessage").fadeOut('slow');
-            }, 3000);
-        });
+    setTimeout(function(){
+      $("#loginErrorMessage").fadeOut('slow');
+    }, 3000);
+  });
 </script>
 </body>
 </html>
